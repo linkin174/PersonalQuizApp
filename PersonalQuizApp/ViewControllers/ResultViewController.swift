240358bda5565054873b6ecebfe3616d011ffc8c
Aleksandr Kretov <kretov.1988@gmail.com>
2022-03-16 16:00:05 +0500

    Final

--- PersonalQuizApp/ViewControllers/ResultViewController.swift
+++ PersonalQuizApp/ViewControllers/ResultViewController.swift
@@ -13,29 +13,27 @@ class ResultViewController: UIViewController {
     @IBOutlet var choosenAnimalDescriptionLabel: UILabel!
     
     //MARK: Public Properties
-    var choosenAnswers: [Answer]!
+    var answers: [Answer]!
     
     //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: false)
+        
         view.setGradientBackground(.cyan, .white)
-        choosenAnimalLabel.text = "Вы - \(getAnimalDependingOnAnswers(from: choosenAnswers).rawValue)!"
-        choosenAnimalDescriptionLabel.text = getAnimalDependingOnAnswers(from: choosenAnswers).definition
+        
+        choosenAnimalLabel.text = "Вы - \(getChoosenAnimal(from: answers).rawValue)!"
+        choosenAnimalDescriptionLabel.text = getChoosenAnimal(from: answers).definition
     }
 
     //MARK: Private methods
-    private func getAnimalDependingOnAnswers(from answers: [Answer]) -> Animal {
+    private func getChoosenAnimal(from answers: [Answer]) -> Animal {
         var animals: [Animal: Int] = [:]
         var choosenAnimal: Animal?
-        
-        answers.forEach { animals[$0.animal] = (animals[$0.animal] ?? 0 ) + 1
-            debugPrint(animals)
-        }
-        
-        if let (animal, _) =  animals.max(by: { $0.value < $1.value}) {
-            choosenAnimal = animal
-        }
+
+        answers.forEach({ animals[$0.animal] = (animals[$0.animal] ?? 1 ) + 1 })
+        debugPrint(animals)
+        choosenAnimal = animals.sorted(by: { $0.value > $1.value }).first?.key
         
         return choosenAnimal ?? .dog
     }
